9e72cf1705d32b49df11953bc0fbfa40ab4b605e
Seboostian02 <antique_sebi@yahoo.ro>
2024-03-26 17:52:02 +0200

    Added homepage

---
+++ RetardLessFR-GUI/pages/Homepage.jsx
@@ -0,0 +1,22 @@
+import styles from "./Homepage.module.css";
+import { Link } from "react-router-dom";
+//import PageNav from "../components/PageNav";
+
+export default function Homepage() {
+  return (
+    <main className={styles.homepage}>
+      {/* <PageNav /> */}
+      <section>
+        <h1>
+          RetardLESS
+          <br />
+          Informatii LIVE legate de CTP Iasi.
+        </h1>
+        <h2>O aplicatie facuta dupa TRANZY API.</h2>
+        <Link to="/app" className="cta">
+          Vezi traseele acum
+        </Link>
+      </section>
+    </main>
+  );
+}
