7e94e1c1947741698283957ce9eebfb26a1018e1
Seboostian02 <antique_sebi@yahoo.ro>
2024-03-27 14:33:48 +0200

    Building the first page features

--- RetardLessFR-GUI/src/App.jsx
+++ RetardLessFR-GUI/src/App.jsx
@@ -1,13 +1,20 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Homepage from "../pages/Homepage";
+import PageNotFound from "../pages/PageNotFound";
+import Who from "../pages/Who";
+import Login from "../pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Homepage />} />
-        <Route path="app" element={<h1>da</h1>}></Route>
+
+        <Route path="login" element={<Login />} />
+        <Route path="who" element={<Who />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
