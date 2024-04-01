a3756cb5087dd8f9c25820563102b19c8a9ee841
georgebezis <georgebezis@gmail.com>
2024-04-01 11:47:05 +0300

    gigel

--- Proiect-PIP/src/main/java/ro/tuiasi/ac/Proiect_PIP/App.java
+++ Proiect-PIP/src/main/java/ro/tuiasi/ac/Proiect_PIP/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Sanatate la neamuri!~!");
+        System.out.println("Sanatate la neamurile noastre!~!");
     }
 }
