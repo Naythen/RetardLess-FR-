f3f52f9ca90bb0bbec3e55842324cb893e8575fd
Naythen <sebastian.ncfr@yahoo.com>
2024-04-15 11:33:10 +0300

    Realizare fetch from Tranzy API

--- Proiect-PIP/src/main/java/ro/tuiasi/ac/Proiect_PIP/App.java
+++ Proiect-PIP/src/main/java/ro/tuiasi/ac/Proiect_PIP/App.java
@@ -1,5 +1,15 @@
 package ro.tuiasi.ac.Proiect_PIP;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Response;
+import com.squareup.okhttp.ResponseBody;
+//import com.fasterxml.jackson.databind.ObjectMapper;
 /**
  * Hello world!
  *
@@ -8,11 +18,32 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println("Sanatate la neamuri!~!");
-        System.out.println("Salutari!");
-        System.out.println("Sanatate la neamurile noastre!~!");
+//        System.out.println( "Hello World!" );
+//        System.out.println("Sanatate la neamuri!~!");
+//        System.out.println("Salutari!");
+//        System.out.println("Sanatate la neamurile noastre!~!");
+//        
+//        System.out.println("aici merge");
+        Devazut d = new Devazut();
         
-        System.out.println("aici merge");
+        try {
+            Response response = d.client.newCall(d.request).execute();
+            // Handle the response here
+            ResponseBody responseBody = response.body();
+            String jsonString = responseBody.string();
+            if (responseBody != null) {
+                System.out.println(jsonString);
+            }
+            
+            File file = new File("D:\\work\\1302B\\pipproiect\\testulmeu2.json");
+            FileWriter writer = new FileWriter(file);
+            writer.write(jsonString);
+            writer.close();
+            System.out.println("Content written to the file successfully.");
+        } 
+   	 catch (IOException e) {
+            e.printStackTrace();
+            // Handle the exception, e.g., log it, show an error message, etc.
+        }
     }
 }
