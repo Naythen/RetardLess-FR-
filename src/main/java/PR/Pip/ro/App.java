9f79ae96773e2c414c40c1a7ace7152a9fbe075c
georgebezis <georgebezis31@gmail.com>
2024-05-15 15:29:13 +0300

    Timed request #3

--- src/main/java/PR/Pip/ro/App.java
+++ src/main/java/PR/Pip/ro/App.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import static PR.Pip.ro.Request.timedrequest;
+
 /**
  * Hello world!
  *
@@ -11,31 +13,7 @@ import java.util.concurrent.TimeUnit;
 public class App 
 {
     public static void main(String[] args) throws InterruptedException, IOException {
-        boolean x=true;
-        int displayMinutes=0;
-        long starttime=System.currentTimeMillis();
-        System.out.println("Timer:");
-        while(x)
-        {
-            String s ="D:\\JavaProjects\\untitled1\\Request\\req";
-            Request d=new Request();
-            TimeUnit.SECONDS.sleep(1);
-            long timepassed=System.currentTimeMillis()-starttime;
-            long secondspassed=timepassed/1000;
-            if(secondspassed==60)
-            {
-                secondspassed=0;
-                starttime=System.currentTimeMillis();
-                s=s.concat(Integer.toString(displayMinutes));
-                s=s.concat(".json");
-                File file = new File(s);
-                d.request(file);
-            }
-            if((secondspassed%60)==0)
-                displayMinutes++;
-
-            System.out.println(displayMinutes+"::"+secondspassed);
-        }
+        timedrequest();
 
     }
 
--- src/main/java/PR/Pip/ro/Request.java
+++ src/main/java/PR/Pip/ro/Request.java
@@ -3,6 +3,7 @@ package PR.Pip.ro;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Response;
@@ -29,5 +30,33 @@ public class Request {
         writer.write(jsonString);
         writer.close();
         System.out.println("Content written to the file successfully.");
+	}
+	public static void timedrequest() throws InterruptedException, IOException {
+		boolean x=true;
+		int displayMinutes=0;
+		long starttime=System.currentTimeMillis();
+		System.out.println("Timer:");
+		while(x)
+		{
+			String s ="D:\\JavaProjects\\untitled1\\Request\\req";
+			Request d=new Request();
+			TimeUnit.SECONDS.sleep(1);
+			long timepassed=System.currentTimeMillis()-starttime;
+			long secondspassed=timepassed/1000;
+			if(secondspassed==60)
+			{
+				secondspassed=0;
+				starttime=System.currentTimeMillis();
+				s=s.concat(Integer.toString(displayMinutes));
+				s=s.concat(".json");
+				File file = new File(s);
+				d.request(file);
+			}
+			if((secondspassed%60)==0)
+				displayMinutes++;
+
+			System.out.println(displayMinutes+"::"+secondspassed);
+		}
+
 	}
 }
